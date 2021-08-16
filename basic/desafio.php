a238457ea79ea4283daf9b82b5bb17511b2db752
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-16 20:19:30 -0300

    fix: resolution of exercises

--- basic/desafio.php
+++ basic/desafio.php
@@ -2,7 +2,7 @@
   Desafio
 </div>
 <ul>
-  <li>1 + 1 = ?</li>
-  <li>4 + 4 = ?</li>
-  <li>8 + 8 = ?</li>
+  <li>1 + 1 = <?php echo '2' ?></li>
+  <li>4 + 4 = <?= '8' ?></li>
+  <li>8 + 8 = <?php Print '16' ?></li>
 </ul>
