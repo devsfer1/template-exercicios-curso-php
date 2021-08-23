bfbd31b4b4764a7f05e81686c21bd6e1b6759091
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-23 05:52:01 -0300

    feat: finished desafio_string

--- types/desafio_string.php
+++ types/desafio_string.php
@@ -8,3 +8,12 @@
 // Avaliando os métodos da documentação da string,
 // qual o métodos que a posição do texto 'abc'
 // na string 'AbcaBcabc' retorna 1?
+
+//Minha resposta (esqueci do "!" kk)
+$mystring = 'AbcaBcabc';
+$findme = 'abc';
+$pos = stripos('x'.$mystring, $findme, 1);
+echo $pos . '<br>';
+
+//Resposta do Curso
+echo stripos('!AbcaBcabc', 'abc');
